52017f8e369970a59bb8e9f2508c4ce4f9f0061b
Romas Opulskis <opulskr@gmail.com>
2022-12-02 00:17:49 +0200

    Some refactoring

---
+++ Day1/Helper.cs
@@ -0,0 +1,31 @@
+﻿namespace Day1
+{
+    internal static class Helper
+    {
+        public static List<IElf> FormatData(IEnumerable<string> rawData)
+        {
+            List<IElf> elves = new();
+            IElf elf = null;
+
+            foreach (var line in rawData)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    elf = new Elf();
+                    elves.Add(elf);
+                }
+                else
+                {
+                    if (elf == null)
+                    {
+                        elf = new Elf();
+                        elves.Add(elf);
+                    }
+
+                    elf.AddCalories(int.Parse(line));
+                }
+            }
+            return elves;
+        }
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -4,27 +4,7 @@ using Day1;
 
 string[] rawData = File.ReadAllLines("RawData.txt");
 
-List<IElf> elves = new List<IElf>();
-IElf elf = null;
-
-foreach (var line in rawData)
-{
-    if (string.IsNullOrEmpty(line))
-    {
-        elf = new Elf();
-        elves.Add(elf);
-    }
-    else
-    {
-        if (elf == null)
-        {
-            elf = new Elf();
-            elves.Add(elf);
-        }
-
-        elf.AddCalories(int.Parse(line));
-    }
-}
+List<IElf> elves = Helper.FormatData(rawData);
 
 Console.WriteLine("Part one");
 var maxCalories = elves.Max(e => e.GetTotalCalories());
@@ -35,3 +15,4 @@ Console.WriteLine("Part two");
 var totalCalories = elves.OrderByDescending(e => e.GetTotalCalories()).Take(3).Sum(e=>e.GetTotalCalories());
 Console.WriteLine($"Total calories carried by 3 elves: {totalCalories}");
 Console.ReadLine();
+
